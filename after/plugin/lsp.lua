c0abafd55f05703840784ffbca0fb7791c740184
Rodrez <fabian.rodrez@gmail.com>
2023-06-11 12:32:54 -0500

    Add autoformatters

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,73 +3,110 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  'tsserver',
-  'rust_analyzer',
-  'ruff_lsp',
-  'pyright',
-  'lua_ls'
+	"tsserver",
+	"rust_analyzer",
+	"ruff_lsp",
+	"pyright",
+	"lua_ls",
 })
 
 -- Fix Undefined global 'vim'
-lsp.configure('lua-language-server', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
+lsp.configure("lua-language-server", {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
 })
 
-
-local cmp = require('cmp')
-    local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<Tab>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<S-Tab>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+	["<Tab>"] = cmp.mapping.select_prev_item(cmp_select),
+	["<S-Tab>"] = cmp.mapping.select_next_item(cmp_select),
+	["<CR>"] = cmp.mapping.confirm({ select = true }),
+	["<C-Space>"] = cmp.mapping.complete(),
 })
-
 
 lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
+	mapping = cmp_mappings,
 })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
-    -- sign_icons = {
-    --     error = 'E',
-    --     warn = 'W',
-    --     hint = 'H',
-    --     info = 'I'
-    -- }
+	suggest_lsp_servers = false,
+	-- sign_icons = {
+	--     error = 'E',
+	--     warn = 'W',
+	--     hint = 'H',
+	--     info = 'I'
+	-- }
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+	local opts = { buffer = bufnr, remap = false }
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>lca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>le", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+	vim.keymap.set("n", "gd", function()
+		vim.lsp.buf.definition()
+	end, opts)
+	vim.keymap.set("n", "K", function()
+		vim.lsp.buf.hover()
+	end, opts)
+	vim.keymap.set("n", "<leader>vws", function()
+		vim.lsp.buf.workspace_symbol()
+	end, opts)
+	vim.keymap.set("n", "<leader>vd", function()
+		vim.diagnostic.open_float()
+	end, opts)
+	vim.keymap.set("n", "[d", function()
+		vim.diagnostic.goto_next()
+	end, opts)
+	vim.keymap.set("n", "]d", function()
+		vim.diagnostic.goto_prev()
+	end, opts)
+	vim.keymap.set("n", "<leader>lca", function()
+		vim.lsp.buf.code_action()
+	end, opts)
+	vim.keymap.set("n", "<leader>le", function()
+		vim.lsp.buf.references()
+	end, opts)
+	vim.keymap.set("n", "<leader>lr", function()
+		vim.lsp.buf.rename()
+	end, opts)
+	vim.keymap.set("i", "<C-h>", function()
+		vim.lsp.buf.signature_help()
+	end, opts)
 end)
 
 lsp.format_on_save({
-  format_opts = {
-    async = false,
-    timeout_ms = 10000,
-  },
+	format_opts = {
+		async = false,
+		timeout_ms = 10000,
+	},
+	servers = {
+		-- ['lua_ls'] = {'lua'},
+		-- ['rust_analyzer'] = {'rust'},
+		-- if you have a working setup with null-ls
+		-- you can specify filetypes it can format.
+		["null-ls"] = { "javascript", "typescript", "python", "lua" },
+	},
 })
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true
+	virtual_text = true,
 })
 
+local null_ls = require("null-ls")
+
+null_ls.setup({
+	sources = {
+		-- Replace these with the tools you have installed
+		-- make sure the source name is supported by null-ls
+		-- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.black,
+	},
+})
--- lua/mask/lazy.lua
+++ lua/mask/lazy.lua
@@ -12,11 +12,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = " " -- sets map leader to space
-
 local plugins = {
 
-	{ "folke/neoconf.nvim",                     cmd = "Neoconf" },
 	-- Themes
 	{
 		"folke/tokyonight.nvim",
@@ -24,75 +21,77 @@ local plugins = {
 		priority = 1000,
 	},
 	-- Must haves
-	{ "nvim-treesitter/nvim-treesitter",        run = ":TSUpdate" },
+	{ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" },
 	{ "nvim-treesitter/nvim-treesitter-context" },
-    { "theprimeagen/harpoon" },
-    { "theprimeagen/refactoring.nvim" },
-    { "theprimeagen/vim-with-me" },
-     {
-          'nvim-telescope/telescope.nvim',
-          dependencies = { 'nvim-lua/plenary.nvim'}
-      },
-	-- find the keys
-{
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  },
-  { "folke/trouble.nvim",
- dependencies = { "nvim-tree/nvim-web-devicons" },
-},
-	{ "folke/zen-mode.nvim",                    lazy = true },
-
-    -- LSP
+	{ "theprimeagen/harpoon" },
+	{ "theprimeagen/refactoring.nvim" },
+	{ "theprimeagen/vim-with-me" },
 	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v1.x',
-		dependencies = {
-			-- LSP Support
-			{ 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
-
-			-- Autocompletion
-			{ 'hrsh7th/nvim-cmp' },
-			{ 'hrsh7th/cmp-buffer' },
-			{ 'hrsh7th/cmp-path' },
-			{ 'saadparwaiz1/cmp_luasnip' },
-			{ 'hrsh7th/cmp-nvim-lsp' },
-			{ 'hrsh7th/cmp-nvim-lua' },
-
-			-- Snippets
-			{ 'L3MON4D3/LuaSnip' },
-			{ 'rafamadriz/friendly-snippets' },
-		},
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"numToStr/Comment.nvim",
 		config = function()
-			require('lsp-zero').setup()
+			require("Comment").setup()
 		end,
 	},
-    {'numToStr/Comment.nvim',
-    config = function()
-            require('Comment').setup()
-        end
-    },
-    {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
-    }
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 600
+		end,
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+	{ "folke/trouble.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
+	{ "folke/zen-mode.nvim", lazy = true },
+
+	-- LSP
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		-- branch = "v1.x",
+		dependencies = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+			{ "jay-babu/mason-null-ls.nvim" },
+
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
+
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+
+			--Formatting
+			{ "jose-elias-alvarez/null-ls.nvim" },
+		},
+		config = function()
+			require("lsp-zero").setup()
+		end,
+	},
 }
 
 require("lazy").setup(plugins, {})
