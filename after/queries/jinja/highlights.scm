6271363e00e439d0ebfbacac91d5c46dc74788a5
Rodrez <fabian.rodrez@gmail.com>
2024-04-11 21:39:08 -0500

    tree sitter jinja work

---
+++ after/queries/jinja/highlights.scm
@@ -0,0 +1,8 @@
+(expression) @expression
+(statement) @variable.builtin
+(keyword) @keyword
+(identifier) @parameter
+(operator) @operator
+(string) @string
+(expression_begin) @braces
+(expression_end) @braces
